66a50839618a1201618f303a2607e483f90a60ed
hsh0367 <hsh0367@gmail.com>
2021-11-16 17:43:06 +0900

    add: default vue, nuxt file

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="page-wrapper">
+    <div class="">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import alticleCardBlock from "~/components/blocks/AlticleCardBlock"
+export default {
+  components :  {alticleCardBlock},
+}
+</script>
