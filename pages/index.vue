f594c61b1755206aeac0bee3e77d2f904562084a
hsh0367 <hsh0367@gmail.com>
2021-11-17 04:59:24 +0900

    fix: add new code

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,52 @@
 <template>
   <div class="page-wrapper">
-    <div class="">
-      
-    </div>
+    <template v-if="$fetchState.pending">
+      <div class="article-cards-wrapper">
+        <content-placeholders
+          v-for="p in 30"
+          :key="p"
+          rounded
+          class="article-card-block"
+        >
+          <content-placeholders-img />
+          <content-placeholders-text :lines="3" />
+        </content-placeholders>
+      </div>
+    </template>
+    <template v-else-if="$fetchState.error">
+      <p>{{ $fetchState.error.message }}</p>
+    </template>
+    <template v-else>
+      <div class="article-cards-wrapper">
+        <article-card-block
+          v-for="(article, i) in articles"
+          :key="article.id"
+          v-observe-visibility="
+            i === articles.length - 1 ? lazyLoadArticles : false
+          "
+          :article="article"
+          class="article-card-block"
+        />
+      </div>
+    </template>
   </div>
 </template>
-
 <script>
-import alticleCardBlock from "~/components/blocks/AlticleCardBlock"
-export default {
-  components :  {alticleCardBlock},
-}
+  import alticleCardBlock from "~/components/blocks/AlticleCardBlock"
+  import _article from './_username/_article.vue'
+  export default {
+    components :  {alticleCardBlock, _article},
+    data() {
+      return {
+        currentPage: 1,
+        articles: []
+      }
+    },
+    async fetch(){
+      const articles = await fetch(
+        `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
+      ).then(res => res.json())
+      this.articles = this.articles.concat(articles)
+    }
+  }
 </script>
